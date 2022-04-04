4cfacface8d0b297fc8bfd0550de0149d729a3db
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-04-04 20:38:48 +0530

    Add files via upload

---
+++ src/requests.js
@@ -0,0 +1,19 @@
+
+
+//got this key from tmdb.com
+const API_KEY="ad674b6e2968cbffd55ab3114a9228d2";
+
+//from documentation of tmdb
+const requests = {
+
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+  }
+
+  export default requests;
